04cfb4d051e7c5cd6379172926696581029c33db
Bipu Bajgai <bipu@Bipus-MacBook-Pro.local>
2019-06-04 21:59:54 +0800

    Codes

---
+++ naivebayes.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#================
+# LIBRARIES USED
+#================
+import pandas as pandas
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import model_selection
+
+#================================
+# SETTING SOME GLOBAL VARIABLES
+#================================
+trainingSize = 0.3
+randomSeed = 7
+
+#====================
+# READING THE DATASET
+#====================
+dataset = pandas.read_csv('./iris.data', sep=',', names=["Sepal Length(cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Category"])
+
+#==============================================
+# REPLACING THE SPECIES OR CATEGORY OF FLOWERS
+#==============================================
+dataset.Category.replace(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], [1, 2, 3], inplace=True)
+
+
+#=================================
+# STRING VALUES IN ARRAY, X AND Y
+#=================================
+dataArray = dataset.values
+X = dataArray[:,0:4]
+Y = dataArray[:,4]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=trainingSize, random_state=randomSeed)
+
+
+#========================
+# NAÏVE BAYES CLASSIFIER
+#========================
+classifier = GaussianNB()
+classifier.fit(X_train, Y_train)
+
+# Predicting test dataset result
+predictionClassifier = classifier.predict(X_test)
+
+# Creating confusion matrix
+confusionMatrix = confusion_matrix(Y_test, predictionClassifier)
+
+# Accuracy of model
+accuracy = accuracy_score(Y_test, predictionClassifier)
+
+
+print '\n'
+print 'Following is the confusion matrix'
+print '================================='
+print ' '
+print confusionMatrix
+print '\n'
+print '\n'
+print 'Following is the accuracy of classifier'
+print '======================================='
+print accuracy
+print '\n'
+
+
+#========================
+# CROSS VALIDATION
+#========================
+kFold = model_selection.KFold(n_splits=10, random_state=randomSeed)
+
+validationResults = model_selection.cross_val_score(GaussianNB(), X_train, Y_train, cv=kFold, scoring='accuracy')
+
+
+
+print 'Mean value of prediction'
+print '========================'
+print validationResults.mean()
+print '\n'
+print '\n'
+print 'Standard Deviation of prediction'
+print '================================'
+print validationResults.std()
+
+
